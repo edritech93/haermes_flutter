45f8ced9797acf3099875a3ae2dde53a08dfa8ef
Yudi Edri Alviska <yudi.a@weefer.co.id>
2022-02-11 14:23:41 +0700

    [Login] - add validator

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,6 +10,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final TextEditingController _controllerUsername = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   String username = '';
   String password = '';
 
@@ -22,6 +23,32 @@ class _LoginState extends State<Login> {
   void dispose() {
     _controllerUsername.dispose();
     super.dispose();
+  }
+
+  void _onChangeUsername(String value) {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        username = value;
+      });
+    }
+  }
+
+  void _onChangePassword(String value) {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        password = value;
+      });
+    }
+  }
+
+  void _onPressLogin(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
   }
 
   @override
@@ -40,43 +67,61 @@ class _LoginState extends State<Login> {
                         image: AssetImage('assets/images/icon_haermes.png')),
                     margin: const EdgeInsets.fromLTRB(0, 100, 0, 54),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 16),
-                    child: TextField(
-                        controller: _controllerUsername,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Username',
-                        ),
-                        onChanged: (String value) => setState(() {
-                              username = value;
-                            })),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 24),
-                    child: TextField(
-                        controller: _controllerPassword,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Password',
-                        ),
-                        onChanged: (String value) => setState(() {
-                              password = value;
-                            })),
-                  ),
-                  Container(
-                    child: ElevatedButton(
-                        onPressed: (() {}),
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(fontSize: 21),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50),
-                        )),
-                    margin: const EdgeInsets.only(bottom: 16),
-                  ),
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 16),
+                            child: TextFormField(
+                                controller: _controllerUsername,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Username',
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Username is required';
+                                  }
+                                  return null;
+                                },
+                                onChanged: (String value) =>
+                                    {_onChangeUsername(value)}),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 24),
+                            child: TextFormField(
+                                controller: _controllerPassword,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Password',
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Password is required';
+                                  }
+                                  return null;
+                                },
+                                onChanged: (String value) =>
+                                    {_onChangePassword(value)}),
+                          ),
+                          Container(
+                            child: ElevatedButton(
+                                onPressed: (() {
+                                  _onPressLogin(context);
+                                }),
+                                child: const Text(
+                                  'Login',
+                                  style: TextStyle(fontSize: 21),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size.fromHeight(50),
+                                )),
+                            margin: const EdgeInsets.only(bottom: 16),
+                          ),
+                        ],
+                      )),
                 ],
               ),
             )));
