3b5b1eb4bdf0a489e53fb307b0baab03585fbc1c
Yudi Edri Alviska <yudi.a@weefer.co.id>
2022-02-11 14:10:52 +0700

    add asset to screen login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -31,48 +31,54 @@ class _LoginState extends State<Login> {
           title: const Text('Login'),
         ),
         body: Container(
-          margin: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 16),
-                child: TextField(
-                    controller: _controllerUsername,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Username',
-                    ),
-                    onChanged: (String value) => setState(() {
-                          username = value;
-                        })),
+            margin: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    child: const Image(
+                        image: AssetImage('assets/images/icon_haermes.png')),
+                    margin: const EdgeInsets.fromLTRB(0, 100, 0, 54),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 16),
+                    child: TextField(
+                        controller: _controllerUsername,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Username',
+                        ),
+                        onChanged: (String value) => setState(() {
+                              username = value;
+                            })),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 24),
+                    child: TextField(
+                        controller: _controllerPassword,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                        onChanged: (String value) => setState(() {
+                              password = value;
+                            })),
+                  ),
+                  Container(
+                    child: ElevatedButton(
+                        onPressed: (() {}),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(fontSize: 21),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50),
+                        )),
+                    margin: const EdgeInsets.only(bottom: 16),
+                  ),
+                ],
               ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 16),
-                child: TextField(
-                    controller: _controllerPassword,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                    onChanged: (String value) => setState(() {
-                          password = value;
-                        })),
-              ),
-              Container(
-                child: ElevatedButton(
-                    onPressed: (() {}),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(fontSize: 21),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                    )),
-                margin: const EdgeInsets.only(bottom: 16),
-              ),
-            ],
-          ),
-        ));
+            )));
   }
 }
